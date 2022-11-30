882b51dce917458a9085201e8f273bb9dd98325b
7611Sumer <87403522+7611Sumer@users.noreply.github.com>
2022-11-30 16:27:04 +0530

    Add files via upload

---
+++ rolladice.py
@@ -0,0 +1,13 @@
+import random
+min = 1
+max = 6
+response = "yes"
+while response == "y":
+    print "The values are..."
+    print random.randint(min,max)
+    if 
+    response = input("want to roll the dices again?:") 
+    
+    #Mam what do i need to do ahead ?? and why the code is giving a error i am not able to understand
+
+    
